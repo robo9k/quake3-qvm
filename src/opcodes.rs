44246c3ecb19b9614320574e29d0b590ff3e1239
robo9k <robo@9k.lv>
2016-12-19 14:19:07 +0100

    Add all ioquake3 opcodes

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,3 +1,83 @@
+/// Operation code for a QVM instruction.
+///
+/// See ioquake3's `opcode_t` in `qcommon/vm_local.h`
 pub enum Opcode {
-    UNDEF = 0,
+    UNDEF,
+
+    IGNORE,
+
+    BREAK,
+
+    ENTER,
+    LEAVE,
+    CALL,
+    PUSH,
+    POP,
+
+    CONST,
+    LOCAL,
+
+    JUMP,
+
+    EQ,
+    NE,
+
+    LTI,
+    LEI,
+    GTI,
+    GEI,
+
+    LTU,
+    LEU,
+    GTU,
+    GEU,
+
+    EQF,
+    NEF,
+
+    LTF,
+    LEF,
+    GTF,
+    GEF,
+
+    LOAD1,
+    LOAD2,
+    LOAD4,
+    STORE1,
+    STORE2,
+    STORE4,
+    ARG,
+
+    BLOCK_COPY,
+
+    SEX8,
+    SEX16,
+
+    NEGI,
+    ADD,
+    SUB,
+    DIVI,
+    DIVU,
+    MODI,
+    MODU,
+    MULI,
+    MULU,
+
+    BAND,
+    BOR,
+    BXOR,
+    BCOM,
+
+    LSH,
+    RSHI,
+    RSHU,
+
+    NEGF,
+    ADDF,
+    SUBF,
+    DIVF,
+    MULF,
+
+    CVIF,
+    CVFI,
 }
