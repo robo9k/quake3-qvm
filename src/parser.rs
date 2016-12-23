2477ee4e0118e760b8b4eb4ff7cb2108cfe6bd8a
robo9k <robo@9k.lv>
2016-12-23 15:32:40 +0100

    Add op_enter parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use super::{opcodes, bytecode};
+use nom::*;
 
 type Input = u8;
 type InputSlice<'a> = &'a [Input];
@@ -10,10 +11,17 @@ named!(op_break<InputSlice,bytecode::Instruction>,
     )
 );
 
+named!(op_enter<InputSlice,bytecode::Instruction>,
+    do_parse!(
+        tag!([opcodes::Opcode::ENTER as Input]) >>
+        frame_size: le_u32             >>
+        (bytecode::Instruction::ENTER(frame_size))
+    )
+);
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use nom::*;
 
     #[test]
     fn test_op_break_exact_match() {
@@ -27,6 +35,13 @@ mod tests {
         let data = [0x0];
         let result = op_break(&data);
         assert_eq!(result, IResult::Error(ErrorKind::Tag));
+    }
+
+    #[test]
+    fn test_op_enter_exact_match() {
+        let data = [0x3, 0x42, 0x0, 0x0, 0x0];
+        let result = op_enter(&data);
+        assert_eq!(result, IResult::Done(&b""[..], bytecode::Instruction::ENTER(0x42)));
     }
 
 }
