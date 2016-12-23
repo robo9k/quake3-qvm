0bdba028b9bc065229adfd86d22cc623b65c0c08
robo9k <robo@9k.lv>
2016-12-23 18:06:06 +0100

    Add remaining instruction parsers

--- src/parser.rs
+++ src/parser.rs
@@ -31,6 +31,12 @@ macro_rules! op_u32 {
     }
 }
 
+macro_rules! op_u8 {
+    ($name:ident, $op:path, $ins:path) => {
+        op_fn!($name, $op, le_u8, $ins);
+    }
+}
+
 
 op!(op_undef, Opcode::UNDEF, Instruction::UNDEF);
 
@@ -70,6 +76,47 @@ op_u32!(op_lef, Opcode::LEF, Instruction::LEF);
 op_u32!(op_gtf, Opcode::GTF, Instruction::GTF);
 op_u32!(op_gef, Opcode::GEF, Instruction::GEF);
 
+op!(op_load1, Opcode::LOAD1, Instruction::LOAD1);
+op!(op_load2, Opcode::LOAD2, Instruction::LOAD2);
+op!(op_load4, Opcode::LOAD4, Instruction::LOAD4);
+op!(op_store1, Opcode::STORE1, Instruction::STORE1);
+op!(op_store2, Opcode::STORE2, Instruction::STORE2);
+op!(op_store4, Opcode::STORE4, Instruction::STORE4);
+op_u8!(op_arg, Opcode::ARG, Instruction::ARG);
+
+op_u32!(op_block_copy, Opcode::BLOCK_COPY, Instruction::BLOCK_COPY);
+
+op!(op_sex8, Opcode::SEX8, Instruction::SEX8);
+op!(op_sex16, Opcode::SEX16, Instruction::SEX16);
+
+op!(op_negi, Opcode::NEGI, Instruction::NEGI);
+op!(op_add, Opcode::ADD, Instruction::ADD);
+op!(op_sub, Opcode::SUB, Instruction::SUB);
+op!(op_divi, Opcode::DIVI, Instruction::DIVI);
+op!(op_divu, Opcode::DIVU, Instruction::DIVU);
+op!(op_modi, Opcode::MODI, Instruction::MODI);
+op!(op_modu, Opcode::MODU, Instruction::MODU);
+op!(op_muli, Opcode::MULI, Instruction::MULI);
+op!(op_mulu, Opcode::MULU, Instruction::MULU);
+
+op!(op_band, Opcode::BAND, Instruction::BAND);
+op!(op_bor, Opcode::BOR, Instruction::BOR);
+op!(op_bxor, Opcode::BXOR, Instruction::BXOR);
+op!(op_bcom, Opcode::BCOM, Instruction::BCOM);
+
+op!(op_lsh, Opcode::LSH, Instruction::LSH);
+op!(op_rshi, Opcode::RSHI, Instruction::RSHI);
+op!(op_rshu, Opcode::RSHU, Instruction::RSHU);
+
+op!(op_negf, Opcode::NEGF, Instruction::NEGF);
+op!(op_addf, Opcode::ADDF, Instruction::ADDF);
+op!(op_subf, Opcode::SUBF, Instruction::SUBF);
+op!(op_divf, Opcode::DIVF, Instruction::DIVF);
+op!(op_mulf, Opcode::MULF, Instruction::MULF);
+
+op!(op_cvif, Opcode::CVIF, Instruction::CVIF);
+op!(op_cvfi, Opcode::CVFI, Instruction::CVFI);
+
 
 #[cfg(test)]
 mod tests {
@@ -96,4 +143,10 @@ mod tests {
         assert_eq!(result, IResult::Done(&b""[..], Instruction::ENTER(0x42)));
     }
 
+    #[test]
+    fn test_op_arg_exact_match() {
+        let data = [0x21, 0x42];
+        let result = op_arg(&data);
+        assert_eq!(result, IResult::Done(&b""[..], Instruction::ARG(0x42)));
+    }
 }
