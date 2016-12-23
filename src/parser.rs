1d4ff57c029757d3b18959ab5ff2a8c167ca0d72
robo9k <robo@9k.lv>
2016-12-23 16:05:52 +0100

    Add even more generic macros to parse opcodes

--- src/parser.rs
+++ src/parser.rs
@@ -13,22 +13,28 @@ macro_rules! op {
     }
 }
 
-op!(op_break, opcodes::Opcode::BREAK, bytecode::Instruction::BREAK);
-
-macro_rules! op_u32 {
-    ($name:ident, $op:path, $ins:path) => {
+macro_rules! op_fn {
+    ($name:ident, $op:path, $op_fn:ident, $ins:path) => {
         named!($name<InputSlice,bytecode::Instruction>,
             do_parse!(
                 tag!([$op as Input]) >>
-                operand: le_u32      >>
+                operand: $op_fn      >>
                 ($ins(operand))
             )
         );
     }
-
 }
 
+macro_rules! op_u32 {
+    ($name:ident, $op:path, $ins:path) => {
+        op_fn!($name, $op, le_u32, $ins);
+    }
+}
+
+
+op!(op_break, opcodes::Opcode::BREAK, bytecode::Instruction::BREAK);
 op_u32!(op_enter, opcodes::Opcode::ENTER, bytecode::Instruction::ENTER);
+
 
 #[cfg(test)]
 mod tests {
@@ -38,7 +44,8 @@ mod tests {
     fn test_op_break_exact_match() {
         let data = [0x2];
         let result = op_break(&data);
-        assert_eq!(result, IResult::Done(&b""[..], bytecode::Instruction::BREAK));
+        assert_eq!(result,
+                   IResult::Done(&b""[..], bytecode::Instruction::BREAK));
     }
 
     #[test]
@@ -52,7 +59,8 @@ mod tests {
     fn test_op_enter_exact_match() {
         let data = [0x3, 0x42, 0x0, 0x0, 0x0];
         let result = op_enter(&data);
-        assert_eq!(result, IResult::Done(&b""[..], bytecode::Instruction::ENTER(0x42)));
+        assert_eq!(result,
+                   IResult::Done(&b""[..], bytecode::Instruction::ENTER(0x42)));
     }
 
 }
